14f02fe445dd8f5acb687cbe773dbedbb713ced3
alimran halmamat <mr.uygur1996@gmail.com>
2021-02-19 21:33:28 -0800

    added Models and super user

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #media folder base directory
+
+#When someone is accessing the image
+MEDIA_URL = '/media/'
--- portfolio/portfolio_app/admin.py
+++ portfolio/portfolio_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+
+#code for showing model in the admin page as super user
+admin.site.register(Job)
