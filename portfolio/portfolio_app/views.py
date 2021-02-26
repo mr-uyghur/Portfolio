2b1afd4b45720bd39e7c318b86006a568381cef5
alimran halmamat <mr.uygur1996@gmail.com>
2021-02-25 17:30:26 -0800

    improved home page and data to the front page

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 # def index(request):
 #     return HttpResponse("this is the equivalent of @app.route('/')!")
 def home(request):
-    return render(request, "home.html")
+    jobs = Job.objects
+    context = {
+        'jobs': jobs
+    }
+    return render(request, "home.html",context)
